aa87587f161461c5f47cd82d31faff3213038e82
Kasador <huntershaw23@gmail.com>
2019-07-10 15:42:41 -0500

    TEST: added my name.

--- src/Main/App.js
+++ src/Main/App.js
@@ -4,7 +4,8 @@ import '../Main/App.scss';
 function App() {
     return (
         <div className="App">
-            <h1>test connection to dev branch</h1>
+            <h1>test connection to dev branch></h1>
+            <h2>Steven test!</h2>
         </div>
     );
 }
