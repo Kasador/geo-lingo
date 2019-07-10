fdabb208cf7b281812438d0860f966d69b4875d3
Kasador <huntershaw23@gmail.com>
2019-07-10 15:56:49 -0500

    Learned github.

--- src/Main/App.js
+++ src/Main/App.js
@@ -4,9 +4,7 @@ import '../Main/App.scss';
 function App() {
     return (
         <div className="App">
-            <h1>test connection to dev branch</h1>
-            <h1>Tyler added a line</h1>
-            <h3>THis is my updated text! wooo! </h3>
+            <h1>test</h1>
         </div>
     );
 }
