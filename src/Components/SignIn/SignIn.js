f6c2d496aa04139efa78c93e67479e3be4bc16fa
Tyler Valley <tvalley93@gmail.com>
2019-07-10 16:40:24 -0500

    Added react-router and created an empty sign-in page component

---
+++ src/Components/SignIn/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const signIn = () => {
+
+    return (
+        <h1>Sign-In</h1>
+    )
+}
+
+export default signIn;
--- src/Main/App.js
+++ src/Main/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../Main/App.scss';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import SignIn from '../Components/SignIn/SignIn';
 
 function App() {
     return (
         <div className="App">
-            <h1>test</h1>
+
+            
+            <Router>
+                
+                <Link to="/sign-in/">Sign In</Link> {/* temporary sign in button, Link will probably have to be used in Steven's Login component page */}
+                <Route path="/sign-in/" component={SignIn} />
+            </Router>
         </div>
     );
 }
