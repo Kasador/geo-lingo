b4e15d2aa0c1484875f112fae2d339d39ae593f1
Tyler Valley <tvalley93@gmail.com>
2019-07-11 16:40:07 -0500

    basic styling that we'll fix later

--- src/Components/CreateAccount/CreateAccount.js
+++ src/Components/CreateAccount/CreateAccount.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './CreateAccount.scss';
+import { Link } from 'react-router-dom';
 
 
 class CreateAccount extends Component {
@@ -57,12 +58,26 @@ class CreateAccount extends Component {
     submitLanguage = (e) => {
         e.preventDefault(); 
        
-        let addedLanguage = this.state.learningLanguagesList.concat(this.state.learningLanguage); //creates a copy of the languages array in state and adds the new language in
+        let updatedLanguages = this.state.learningLanguagesList.concat(this.state.learningLanguage); //creates a copy of the languages array in state and adds the new language in
 
-        this.setState({learningLanguagesList: addedLanguage}); //replace old array with new array
+        this.setState({learningLanguage: '', learningLanguagesList: updatedLanguages}); //replace old array with new array and reset learning language input.
         
     }
 
+    //when the x is clicked, remove selected language from state
+    deleteLanguage = (e, language) => {
+
+        e.preventDefault();
+        this.setState(prevState => ({
+            learningLanguagesList: prevState.learningLanguagesList.filter(el => el !== language) //keep array items except for specified langage, then update state with filtered array.
+        }));
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        
+        console.log(this.state); // all user info including languages that they want to learn.
+    }
 
     
 
@@ -70,19 +85,27 @@ class CreateAccount extends Component {
 
     render () {
 
+     
+
         // loop through the languages in the state array and output them into a list
         const languages = this.state.learningLanguagesList.map(language => {
             
             return (
-                <li key={language}>{language}</li>
+               <li key={language}>
+                   <h3>{language}</h3>
+                   <button onClick={(e) => this.deleteLanguage(e, language)}>X</button>
+                </li>
             )
         })
         
         return (
             <div className="account-creation-page">
+
+                <Link to="/sign-in/"><button className="back-button">Go Back</button></Link>
+
                 <h1>Create an Account</h1>
 
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit} className="form">
 
                     <div>
                         <label>Username: </label>    
@@ -100,7 +123,7 @@ class CreateAccount extends Component {
                     </div>
 
                     <div>
-                        <label>Email: </label>
+                        <label>Email Address: </label>
                         <input type="text" value={this.state.email} onChange={(e) => this.handleUserInfoChange(e, 'email')}/>
                     </div>
 
@@ -109,21 +132,33 @@ class CreateAccount extends Component {
                         <input type="text" value={this.state.nativeLanguage} onChange={(e) => this.handleUserInfoChange(e, 'nativeLanguage')} />
                     </div>
 
-                    <div>
+                    {/* the part below contains the whole learning languages section */}
+
+                    <div className="languages-input">
                         <label>Languages you want to learn:</label>
+
                         <input type="text" value={this.state.learningLanguage} onChange={(e) => this.handleUserInfoChange(e, 'learningLanguage')} />
+
                         <button onClick={this.submitLanguage}>Add Language</button>
+
+                        {/* shows the languages added */}
+                        <div className="learning-languages">
+                            <ul>
+                                {languages}
+                            </ul>
+                        </div>
+
                     </div>
 
+                    <div>
+                        {/*Submits the entire form */}
+                        <button type="submit">Submit</button>
+                    </div>
 
+                    
                 </form>
 
-                {/* shows the languages added */}
-                <div className="learning-languages">
-                    <ul>
-                        {languages}
-                    </ul>
-                </div>
+                
             </div>
         )
     }
